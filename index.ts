5e3c5292a1e01f120337e80efa236a85655217ca
Masterjoona <69722179+Masterjoona@users.noreply.github.com>
2024-08-04 18:48:12 +0300

    Add support for message contents

--- index.ts
+++ index.ts
@@ -8,6 +8,11 @@ import { definePluginSettings } from "@api/Settings";
 import definePlugin, { OptionType } from "@utils/types";
 
 const settings = definePluginSettings({
+    spoilerWords: {
+        description: "Strings in messages that should be spoilered. Comma separated.",
+        type: OptionType.STRING,
+        default: "",
+    },
     spoilerFilenames: {
         description: "Strings in filenames that should be spoilered. Comma separated.",
         type: OptionType.STRING,
@@ -22,7 +27,6 @@ const settings = definePluginSettings({
         description: "Should the links only be gifs?",
         type: OptionType.BOOLEAN,
         default: true
-
     },
 });
 
@@ -39,23 +43,30 @@ export default definePlugin({
             replacement: [
                 {
                     match: /function \i\((\i),\i\){return(?<=VOICE_MESSAGE.{20,27})/,
-                    replace: "$& $self.shouldSpoiler($1.originalItem.filename) || "
+                    replace: "$& $self.shouldSpoilerFile($1.originalItem.filename) || "
                 },
                 {
                     match: /(\i)=\(0,\i.{10,20};(?=if\((\i).type)/,
-                    replace: "$&$1=$self.spoilerLink($1,$2.url,$2.type);"
+                    replace: "$&$1=$self.shouldSpoilerLink($1,$2.url,$2.type);"
                 }
             ]
+        },
+        {
+            find: ".Messages.MESSAGE_UNSUPPORTED,",
+            replacement: {
+                match: /function \i\((\i),\i\){/,
+                replace: "$&$1.content=$self.shouldSpoilerWords($1.content);"
+            }
         }
     ],
     settings,
-    shouldSpoiler(filename: string): string | null{
+    shouldSpoilerFile(filename: string): string | null {
         const { spoilerFilenames } = settings.store;
         if (!filename || !spoilerFilenames) return null;
         const strings = spoilerFilenames.split(",").map(s => s.trim());
         return strings.some(s => filename.includes(s)) ? "spoiler" : null;
     },
-    spoilerLink(alreadySpoilered: string, link: string, type: string): string | null {
+    shouldSpoilerLink(alreadySpoilered: string, link: string, type: string): string | null {
         if (alreadySpoilered) return alreadySpoilered;
         const { spoilerLinks, gifSpoilersOnly } = settings.store;
         if (!link || !spoilerLinks) return null;
@@ -68,5 +79,21 @@ export default definePlugin({
         } else {
             return isLinkSpoiler ? "spoiler" : null;
         }
+    },
+    shouldSpoilerWords(content: string): string {
+        const { spoilerWords } = settings.store;
+        if (!content || !spoilerWords) return content;
+
+        const strings = spoilerWords.split(",").map(s => s.trim());
+
+        return strings.reduce((acc, s) => {
+            const spoilerRegex = new RegExp(`\\|\\|${s}\\|\\|`, "gi");
+            if (spoilerRegex.test(acc)) {
+                return acc;
+            }
+            const wordRegex = new RegExp(`\\b${s}\\b`, "gi");
+            return acc.replace(wordRegex, `||${s}||`);
+        }, content);
     }
+
 });
